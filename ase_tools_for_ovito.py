16634bf35cf3125131b13277f3b7830ff9466afe
Miguel Caro <mcaroba@gmail.com>
2025-06-20 18:08:31 +0300

    Added support for bonding cutoffs to Ovito helper

--- ase_tools_for_ovito.py
+++ ase_tools_for_ovito.py
@@ -8,11 +8,12 @@ from ovito.io.ase import ase_to_ovito
 from ovito.pipeline import StaticSource
 from ovito.pipeline import Pipeline as OvitoPipeline
 from ovito.data import DataCollection
+from ovito.modifiers import CreateBondsModifier
 
 ###############################################################################################
 #
 class Pipeline:
-    def __init__(self, atoms, radii=None):
+    def __init__(self, atoms, radii=None, cutoffs=None):
         self.atoms = atoms # This is an ASE Atoms object
         # Ovito has trouble with a bunch of array properties assigned to Atoms objects. The fix_atoms
         # property assigned by TurboGAP needs to be removed!
@@ -40,13 +41,35 @@ class Pipeline:
                 numb = atomic_numbers[el]
                 radii[el] = vdw_radii[numb] * 0.3
             self.radii = radii
+        # Set bonding cutoffs
+        if cutoffs is not None:
+            self.cutoffs = cutoffs
+        else:
+            cutoffs = {}
+            for el in el_num:
+                numb = atomic_numbers[el]
+                cutoffs[el] = covalent_radii[numb] * 2.5
+            self.cutoffs = cutoffs
 
         # Modifier functions
+        #
+        # Set the size of the atoms
         def set_particle_size(frame: int, data: DataCollection):
             for el in self.el_num:
                 n = self.el_num[el]
                 data.particles_.particle_types_.type_by_id_(n).radius = self.radii[el]
         pipeline.modifiers.append(set_particle_size)
+        #
+        # Set the cutoff to draw the bonds
+        modifier = CreateBondsModifier(mode=CreateBondsModifier.Mode.Pairwise) # This uses Ovito's own modifier
+        for el in self.el_num:
+            n = self.el_num[el]
+            for el2 in self.el_num:
+                n2 = self.el_num[el2]
+                if n2 >= n:
+                    modifier.set_pairwise_cutoff(n, n2, (self.cutoffs[el]+self.cutoffs[el2])/2.)
+        pipeline.modifiers.append(modifier)
+
 
         # Return Ovito pipeline
         self.pipeline = pipeline
