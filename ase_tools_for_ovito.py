f54169c1afe21180e378356ff67b4dc1fec9eab2
Miguel Caro <mcaroba@gmail.com>
2025-06-21 11:17:36 +0300

    Improved Ovito interface

--- ase_tools_for_ovito.py
+++ ase_tools_for_ovito.py
@@ -9,19 +9,25 @@ from ovito.pipeline import StaticSource
 from ovito.pipeline import Pipeline as OvitoPipeline
 from ovito.data import DataCollection
 from ovito.modifiers import CreateBondsModifier
+from ovito.vis import Viewport, TachyonRenderer
 
 ###############################################################################################
 #
 class Pipeline:
     def __init__(self, atoms, radii=None, cutoffs=None):
         self.atoms = atoms # This is an ASE Atoms object
+
         # Ovito has trouble with a bunch of array properties assigned to Atoms objects. The fix_atoms
         # property assigned by TurboGAP needs to be removed!
         del self.atoms.arrays["fix_atoms"]
-        #
+
+        # Create the data objects
         atoms_data = ase_to_ovito(self.atoms)
         pipeline = OvitoPipeline(source = StaticSource(data = atoms_data)) # This is an Ovito Pipeline
         data = pipeline.compute()
+
+        # Build various system properties
+
         # Build a dictionary of chemical elements
         el_num = {}
         for at in self.atoms:
@@ -32,6 +38,7 @@ class Pipeline:
             el = types.type_by_id_(n).name
             el_num[el] = n
         self.el_num = el_num
+
         # Set atomic radii
         if radii is not None:
             self.radii = radii
@@ -41,6 +48,7 @@ class Pipeline:
                 numb = atomic_numbers[el]
                 radii[el] = vdw_radii[numb] * 0.3
             self.radii = radii
+
         # Set bonding cutoffs
         if cutoffs is not None:
             self.cutoffs = cutoffs
@@ -51,16 +59,28 @@ class Pipeline:
                 cutoffs[el] = covalent_radii[numb] * 2.5
             self.cutoffs = cutoffs
 
-        # Modifier functions
-        #
-        # Set the size of the atoms
-        def set_particle_size(frame: int, data: DataCollection):
+        # Store Ovito pipeline in self to be accessible by modifiers
+        self.pipeline = pipeline
+
+        # Append modifiers
+        self.set_particle_size()
+        self.create_bonds()
+
+    # Modifier functions
+    # TODO: allow for modifier-specific parameter modification (let these functions take
+    #       parameters as input rather than retrieve all of them from the class
+    #       instantiation)
+
+    # Set the size of the atoms
+    def set_particle_size(self):
+        def modifier(frame: int, data: DataCollection):
             for el in self.el_num:
                 n = self.el_num[el]
                 data.particles_.particle_types_.type_by_id_(n).radius = self.radii[el]
-        pipeline.modifiers.append(set_particle_size)
-        #
-        # Set the cutoff to draw the bonds
+        self.pipeline.modifiers.append(modifier)
+
+    # Set the cutoff to draw the bonds
+    def create_bonds(self):
         modifier = CreateBondsModifier(mode=CreateBondsModifier.Mode.Pairwise) # This uses Ovito's own modifier
         for el in self.el_num:
             n = self.el_num[el]
@@ -68,8 +88,21 @@ class Pipeline:
                 n2 = self.el_num[el2]
                 if n2 >= n:
                     modifier.set_pairwise_cutoff(n, n2, (self.cutoffs[el]+self.cutoffs[el2])/2.)
-        pipeline.modifiers.append(modifier)
+        self.pipeline.modifiers.append(modifier)
 
+    # Visualization and rendering options
+    def set_visuals(self, renderer="tachyon", direction=[1,1,1], scale=1., shift=[0,0,0]):
+        if renderer == "tachyon":
+            self.renderer = TachyonRenderer(shadows=False, direct_light_intensity=1.1)
+        else:
+            raise("%s not supported as renderer option")
+        self.vp = Viewport()
+        self.vp.type = Viewport.Type.Perspective
+        self.vp.camera_dir = direction
+        self.vp.camera_pos = shift + self.atoms.get_center_of_mass() - direction / np.dot(direction,direction)**0.5 * scale
 
-        # Return Ovito pipeline
-        self.pipeline = pipeline
+    # Trigger rendering
+    def render(self, filename):
+        self.pipeline.add_to_scene()
+        self.vp.render_image(filename = filename, size=(512,512), alpha=True, renderer=self.renderer)
+        self.pipeline.remove_from_scene()
