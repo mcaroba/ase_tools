96935e3578b79ab55f02a719a5862b40db17c4c7
Miguel Caro <mcaroba@gmail.com>
2026-05-28 13:46:49 +0300

    Made conditional in sh file

--- build_libraries.sh
+++ build_libraries.sh
@@ -8,3 +8,8 @@ python3 -m numpy.f2py --f90flags='-fopenmp' -lgomp -c cluster.f90 -m cluster
 python3 -m numpy.f2py --f90flags='-fopenmp' -lgomp -c surface.f90 -m surface
 
 
+# Build soap_turbo interface here conditionally on having cloned the soap_turbo
+# submodule
+if [[ -d src/soap_turbo ]]; then
+
+fi
